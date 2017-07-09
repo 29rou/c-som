bc141c9bb068b322656c7bb9fbfe860ea8d6a332
29rou <g196010r@icu.ac.jp>
2017-07-09 19:17:09 +0900

    add random take

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 extern crate image;
 use std::path;
-//use std::borrow::Cow;
+extern crate rand;
+use rand::{thread_rng, Rng};
 
-type DataSet <'a>  = Vec<ImgData>;
+type DataSet  = Vec<ImgData>;
+type MiniBatch<'a>  = Vec<&'a ImgData>;
+
+type CSomFilter = [[f32; 9] ;9];
+
+struct CSom {
+    layer_1: CSomFilter,
+    layer_2: CSomFilter
+}
+
+
 
 struct ImgData  {
     path: Box<path::PathBuf>,
@@ -10,46 +21,58 @@ struct ImgData  {
 }
 
 trait HasImg{
-    fn load_img(&self) -> image::RgbImage;
+    fn load_img(&self) -> image::GrayImage;
 }
 
-impl <'a> ImgData{
-    fn new (path:  &path::Path) -> Self{
-        let path = path.to_path_buf();
+impl  ImgData{
+    fn new (path:  path::PathBuf) -> Self{
         let path = Box::new(path);
-        //let path :Cow<'a, path::Path>= Cow::Borrowed(path);
-        let c = path.parent().unwrap().to_str().unwrap().to_string();
-        //let c = path;path::Path::new(path)path::Path::new(path)
-        //let c = c.parent().unwrap().to_str().unwrap();
-        //ImgData{path: path, class: Cow::Borrowed(c)}
+        let c = path.parent().unwrap()
+            .iter().last().unwrap()
+            .to_str().unwrap().to_string();
          ImgData{path: path, class: Box::new(c)}
     }
 }
 
-impl <'a> HasImg for  ImgData{
-    fn load_img(&self) -> image::RgbImage{
-        let jpeg_img = image::open(&self.path.as_path());
-        match jpeg_img{
-            Ok(value) => value.to_rgb(),
-            Err(_) => panic!("Can't read image!!"),
-        }
+impl  HasImg for  ImgData{
+    fn load_img(&self) -> image::GrayImage{
+        let size = 32;
+        let img = match image::open(&self.path.as_path()){
+                Ok(value) => value.to_luma(),
+                Err(_) => panic!("Can't read image!!"),
+            };
+        image::imageops::resize(&img,size,size,image::FilterType::Lanczos3)
     }
 }
 
-fn prepare_dataset<'a> (p: &str) ->  DataSet{
+fn prepare_dataset (p: &str) ->  DataSet{
     extern crate walkdir;
-    let ps = walkdir::WalkDir::new(p)
+    walkdir::WalkDir::new(p)
         .into_iter()
-        .map(|x| ImgData::new(x.unwrap().path()))
-        .collect::<Vec<ImgData>>();
-    ps
+        .map(|x| x.unwrap().path().to_path_buf())
+        .filter(|x| x.is_file())
+        .map(|x| { ImgData::new(x)})
+        .collect::<DataSet>()
 }                                                                               
 
+fn take_n_rand (n: u32 ,dataset :&DataSet)->MiniBatch{
+    let mut rng = thread_rng();
+    let mut v = Vec::new();
+    for _ in 0..n{
+        let r = rng.choose(dataset).unwrap();
+        v.push(r);
+    }
+    v
+}
+
 fn main() {
-    println!("Hello, world!");
-    let path:&str = "/home/yoshiki/Downloads/101_ObjectCategories";
-    let dataset:DataSet = prepare_dataset(path);
-    for entry in dataset{
-       println!("{}",entry.path.display());
+    let path = "/home/yoshiki/Downloads/101_ObjectCategories";
+    let dataset = &prepare_dataset(path);
+    //let minibatch = take_n_rand(100,dataset);
+    for _ in 0..10{
+        for entry in take_n_rand(100,dataset){
+            entry.load_img();
+            println!("{}",entry.class);
+        }
     }
 }
