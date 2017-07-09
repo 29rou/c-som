9bc3d26740ca54e520ea168770cd9aaf31467a1d
29rou <g196010r@icu.ac.jp>
2017-07-09 19:50:51 +0900

    add train

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,10 @@ use rand::{thread_rng, Rng};
 type DataSet  = Vec<ImgData>;
 type MiniBatch<'a>  = Vec<&'a ImgData>;
 
-type CSomFilter = [[f32; 9] ;9];
-
 struct CSom {
-    layer_1: CSomFilter,
-    layer_2: CSomFilter
+    layer_1: [[f32; 9] ;9],
+    layer_2: [[f32; 9] ;9],
+    layer_3: [[[f32; 9]; 5] ;5]
 }
 
 
@@ -57,7 +56,8 @@ fn prepare_dataset (p: &str) ->  DataSet{
 
 fn take_n_rand (n: u32 ,dataset :&DataSet)->MiniBatch{
     let mut rng = thread_rng();
-    let mut v = Vec::new();
+    let mut v :MiniBatch = Vec::new();
+    v.reserve(n as usize);
     for _ in 0..n{
         let r = rng.choose(dataset).unwrap();
         v.push(r);
@@ -65,14 +65,20 @@ fn take_n_rand (n: u32 ,dataset :&DataSet)->MiniBatch{
     v
 }
 
+fn train (n: u32, dataset :&DataSet)-> u32{
+    let minibatch = &take_n_rand(n,dataset);
+    for entry in minibatch{
+        entry.load_img();
+        println!("{}",entry.class);
+    }
+    3
+}
+
 fn main() {
     let path = "/home/yoshiki/Downloads/101_ObjectCategories";
     let dataset = &prepare_dataset(path);
     //let minibatch = take_n_rand(100,dataset);
     for _ in 0..10{
-        for entry in take_n_rand(100,dataset){
-            entry.load_img();
-            println!("{}",entry.class);
-        }
+        train(100, dataset);
     }
 }
