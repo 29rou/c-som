4401154de4a9531508c92584d1f36df6d9d98832
29rou <g196010r@icu.ac.jp>
2017-07-15 09:00:17 +0900

    add get_distances and get_winners

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate image;
-use std::path;
 extern crate rand;
-use rand::{thread_rng, Rng};
+#[macro_use(array)]
 extern crate ndarray;
 extern crate ndarray_rand;
 
@@ -9,7 +8,7 @@ type DataSet  = Vec<ImgData>;
 
 trait DataSetTrait {
     fn new(p:&str)->Self;
-    fn take_n_rand(&self, n:u32)->MiniBatch;
+    fn take_n_rand(&self, n:usize)->MiniBatch;
 }
 
 type MiniBatch<'a>  = Vec<&'a ImgData>;
@@ -21,24 +20,32 @@ struct CSom {
 }
 
 struct ImgData  {
-    path: path::PathBuf,
-    class: String
+    path: std::path::PathBuf,
+    class: String,
 }
 
 impl  ImgData{
-    fn new (path:  path::PathBuf) -> Self{
+    fn new (path:std::path::PathBuf) -> Self{
         let c = path.parent().unwrap()
             .iter().last().unwrap()
             .to_str().unwrap().to_string();
          ImgData{path: path, class: c}
     }
-    fn load_img(&self) -> image::GrayImage{
-        let size = 32;
-        let img = match image::open(&self.path.as_path()){
-                Ok(value) => value.to_luma(),
-                Err(_) => panic!("Can't read image!!"),
-            };
-        image::imageops::resize(&img,size,size,image::FilterType::Lanczos3)
+    fn load_img(&self,size:usize) -> ndarray::Array2<u8>{
+        use ndarray::Array;
+        let img = image::imageops::resize(
+            &image::open(&self.path.as_path())
+            .expect("Can't read image!!")
+            .to_luma(),
+            size as u32,
+            size as u32,
+            image::FilterType::Lanczos3
+        );
+        let img_array = Array::from_shape_vec(
+            (size,size),
+            img.into_vec()
+        ).unwrap();
+        img_array
     }
 }
 
@@ -52,7 +59,8 @@ impl DataSetTrait for DataSet{
             .map(|x| { ImgData::new(x)})
             .collect::<DataSet>()
     } 
-    fn take_n_rand  (&self,n: u32)->MiniBatch{
+    fn take_n_rand  (&self,n: usize)->MiniBatch{
+        use rand::{thread_rng, Rng};
         let mut rng = thread_rng();
         let mut v :MiniBatch = Vec::new();
         v.reserve(n as usize);
@@ -65,38 +73,88 @@ impl DataSetTrait for DataSet{
 }
 
 impl CSom {
-    fn new () ->Self{
-        let get_rand = ||{
-            use rand::distributions::Range;
-            use ndarray::Array;
-            use ndarray_rand::RandomExt;
-            Array::random((9,9),Range::new(0.0, 255.0))
-        };
-        let layer_1 = get_rand();
-        let layer_2 = get_rand();
-        let layer_3 = {
-            use rand::distributions::Range;
-            use ndarray::Array;
-            use ndarray_rand::RandomExt;
-            Array::random((9,9,9),Range::new(0.0, 255.0))
-        };
-        CSom{layer_1: layer_1,layer_2:layer_2,layer_3:layer_3}
+    fn new (kernel:usize) ->Self{
+        use ndarray::Array;;
+        use ndarray_rand::RandomExt;
+        use rand::distributions::Range;
+        let r_dist:Range<f32> = Range::new(0.0, 255.0);
+        CSom{
+            layer_1: Array::random((kernel,kernel),r_dist),
+            layer_2: Array::random((kernel,kernel),r_dist),
+            layer_3: Array::random((kernel,kernel,kernel),r_dist)
+        }
     }
-    fn train (&self, n: u32, dataset :&DataSet){
-        let minibatch = dataset.take_n_rand(n);
-        for entry in minibatch{
-            entry.load_img();
-            println!("{}",entry.class);
+    
+    fn get_conv9(image:ndarray::Array2<u8>) -> ndarray::Array2<f32>{
+        let mut vec:Vec<f32> = Vec::new();
+        let count = image.windows((3,3)).into_iter().count();
+        for kernel in image.windows((3,3)){
+            for entry in &kernel{
+                vec.push(*entry as f32);
+            }
+        }
+        ndarray::Array::from_shape_vec((count,9),vec).unwrap()
+    }
+
+    fn get_distances(&self,imgdata:&ImgData) -> Vec<Vec<(usize,f32)>>{
+        let mut distances = Vec::new();
+        let img = &CSom::get_conv9(imgdata.load_img(32));
+        for kernel in img.genrows(){
+            let mut vec = Vec::new();
+            for (i,cell) in self.layer_1.genrows().into_iter().enumerate(){
+                let diff = &cell - &kernel;
+                let dist = &diff
+                    .map(|x|x.powf(2.0))
+                    .scalar_sum()
+                    .sqrt();
+                vec.push((i,*dist));
+            }
+            distances.push(vec);
+        }
+        distances
+    }
+
+    fn get_winners (&self, imgdata:&ImgData) -> Vec<(usize)>{
+        let distances = self.get_distances(imgdata);
+        let init = (10 as usize, (0.0/0.0));
+        let winers = distances.iter()
+            .map(|x| {
+                x.iter().fold(&init,|m,v| {
+                    if m.1 > v.1{
+                        m
+                    }else{
+                        v
+                    }
+                }).0
+            });
+        winers.collect()
+    }
+
+    fn train (&self, batch_size:usize,train_count: usize, dataset :&DataSet){
+        let minibatchs = std::iter::repeat(())
+            .map(|_| dataset.take_n_rand(batch_size))
+            .take(train_count);//let minibatch = dataset.take_n_rand(n);
+        for (i,minibatch) in minibatchs.enumerate(){
+            for entry in minibatch{
+                let img = self.get_winers(&entry);
+                println!("{}",img.get(1).unwrap());
+            }
         }
     }
 }
 
-
 fn main() {
     let path = "/home/yoshiki/Downloads/101_ObjectCategories";
     let dataset:DataSet = DataSetTrait::new(path);
-    let csom = CSom::new();
-    csom.train(100,&dataset);
-    //let minibatch = take_n_rand(100,dataset);
-    //train(100,&dataset);
+    let csom:CSom = CSom::new(9);
+    /*let img = &dataset.get(4).unwrap().load_img(32);
+    let a = img.subview(ndarray::Axis(0),0);
+    let b = img.subview(ndarray::Axis(0),1);
+    let c = (&a - &b).map(|x|(x.pow(2)as f32)).scalar_sum().sqrt();
+    println!("{}\n\n{}\n\n{}\n\n{}",img,a,b,c);
+    let w =img.windows((3,3));
+    let mut w = w.into_iter();
+    let w = w.next().unwrap();
+    println!("{}",w);*/
+    csom.train(10,100,&dataset);
 }
