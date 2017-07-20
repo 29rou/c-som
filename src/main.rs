cdb592c2b8c3e7f0baecde6f925f4961f0f4f6b4
29rou <g196010r@icu.ac.jp>
2017-07-20 20:47:21 +0900

    remove commentout

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,4 @@ fn main() {
     let dataset:DataSet = DataSetTrait::new(PATH);
     let csom:csom::CSom<f32,U9,U2,U9,U9> = csom::CSom::new();
     csom.train(10,1000,&dataset);
-    //let mut dst = [0,0,0,0];
-    //let src = [1,2,3,4,5,6];
-    //println!("{}",dst.copy_from_slic(&src));
 }
