4202e9d7a37858930dc90387bc7a67875c1321c9
29rou <g196010r@icu.ac.jp>
2017-10-13 17:48:38 +0900

    use iterator

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,9 @@ fn read_cifar(path: &std::path::Path) -> Vec<CifarImage>{
     use std::{fs, mem};
     use std::collections::LinkedList;
     use std::io::{BufReader, Read};
+    use image::GenericImage;
     use self::walkdir::WalkDir;
-    let paths = &WalkDir::new(path)
+    let ref paths = WalkDir::new(path)
         .into_iter()
         .flat_map(|x| x)
         .map(|x| x.path().to_path_buf())
@@ -32,44 +33,51 @@ fn read_cifar(path: &std::path::Path) -> Vec<CifarImage>{
             .filter(|x| !x.is_empty())
             .collect::<Vec<String>>()
     };
-    let mut binary_datas:Vec<u8> = Vec::new();
-    for img_path in paths.iter().filter(|x| x.extension().unwrap()=="bin"){
-        let file = 
-            fs::File::open(img_path)
-            .expect("Can't Open File!!");
-        let mut binary_data:Vec<u8> = Vec::new();
-        BufReader::new(file)
-            .read_to_end(&mut binary_data)
-            .expect("Can't Read File!!");
-        binary_datas.append(&mut binary_data);
-    }
+    let binary_datas:Vec<u8> = paths.iter()
+        .filter(|x| x.extension().unwrap()=="bin")
+        .map(move |x| {
+            let file = fs::File::open(x).expect("Can't Open File!!");
+            let mut binaray_data:Vec<u8> = Vec::new();
+            BufReader::new(file)
+                .read_to_end(&mut binaray_data)
+                .expect("Can't Read File!!");
+            binaray_data
+        })
+        .fold(Vec::new(),move |mut sum,ref mut x| {
+            sum.append(x);
+            sum
+        });
     let mut cifar_images:Vec<CifarImage> = Vec::new();
     {
         let mut binary_datas = &binary_datas[..];
         while !binary_datas.is_empty(){
             let (label,img) = unsafe{
-                use  image::GenericImage;
-                let mut label:[u8;1] = mem::uninitialized();
-                let mut red:[u8;1024] = mem::uninitialized();
-                let mut green:[u8;1024] = mem::uninitialized();
-                let mut blue:[u8;1024] = mem::uninitialized();
-                binary_datas.read_exact(&mut label).unwrap();
-                binary_datas.read_exact(&mut red).unwrap();
-                binary_datas.read_exact(&mut green).unwrap();
-                binary_datas.read_exact(&mut blue).unwrap();
+                let ref mut label:[u8;1] = mem::uninitialized();
+                let ref mut red:[u8;1024] = mem::uninitialized();
+                let ref mut green:[u8;1024] = mem::uninitialized();
+                let ref mut blue:[u8;1024] = mem::uninitialized();
+                binary_datas.read_exact(label).unwrap();
+                binary_datas.read_exact(red).unwrap();
+                binary_datas.read_exact(green).unwrap();
+                binary_datas.read_exact(blue).unwrap();
                 let mut img = image::DynamicImage::new_rgb8(32,32);
                 for y in 0..32{
                     for x in 0..32{
                         let i = x + y * 32;
                         let mut pixel:image::Rgba<u8> = mem::uninitialized();
-                        pixel.data = [red[i],green[i],blue[i],255];
+                        pixel.data = [
+                            *red.get_unchecked(i),
+                            *green.get_unchecked(i),
+                            *blue.get_unchecked(i),
+                            255
+                        ];
                         img.unsafe_put_pixel(x as u32 ,y as u32 ,pixel);
                     }
                 }
-                (label[0],img)
+                (meta_data.get_unchecked(*label.get_unchecked(0) as usize),img)
             };
             let tmp = CifarImage{
-                label: meta_data[label as usize].clone(),
+                label: label.clone(),
                 image: img
             };
             cifar_images.push(tmp);
