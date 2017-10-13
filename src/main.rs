9dd5056b38d4c9c740e326d96ee8ef691fbf1da3
29rou <g196010r@icu.ac.jp>
2017-10-13 17:11:39 +0900

    Read Cifa Aviable

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,81 @@
 extern crate walkdir;
+extern crate image;
 
-fn read_cifar(path: &std::path::Path) -> i32{
+struct CifarImage {
+    label: String,
+    image: image::DynamicImage
+}
+
+fn read_cifar(path: &std::path::Path) -> Vec<CifarImage>{
     use std::{fs, mem};
+    use std::collections::LinkedList;
     use std::io::{BufReader, Read};
     use self::walkdir::WalkDir;
-    struct BinaryImg {
-        label:[u8;1],
-        red:[u8;1024],
-        green:[u8;1024],
-        blue:[u8;1024]
-    } 
-    let paths = WalkDir::new(path)
+    let paths = &WalkDir::new(path)
         .into_iter()
         .flat_map(|x| x)
         .map(|x| x.path().to_path_buf())
         .filter(|x| x.is_file())
-        .collect::<Vec<std::path::PathBuf>>();
-    let meta_paths:Vec<std::path::PathBuf> = (&paths)
-        .into_iter()
-        .by_ref()
-        .filter(|x| x.extension().unwrap()=="txt")
-        .map(|x| x.clone())
-        .collect();
-    let img_paths:Vec<std::path::PathBuf> = paths
-        .into_iter()
-        .filter(move |x| x.extension().unwrap()=="bin")
-        .collect();
-    println!("{:?}",img_paths);
-    let mut imgs:Vec<Vec<BinaryImg>> = Vec::new();
-    for img_path in img_paths{
+        .collect::<LinkedList<std::path::PathBuf>>();
+    let meta_data:Vec<String> = {
+        let meta_path= paths
+            .iter()
+            .find(|x| x.extension().unwrap()=="txt")
+            .expect("Can't Find MetaFile!!");
+        let mut lines = String::new();
+        fs::File::open(meta_path)
+            .expect("Cant Open MetaFile!!")
+            .read_to_string(&mut lines)
+            .expect("Cant Read MetaFile!!");
+        lines.lines()
+            .map(|x| x.to_string())
+            .filter(|x| !x.is_empty())
+            .collect::<Vec<String>>()
+    };
+    let mut binary_datas:Vec<u8> = Vec::new();
+    for img_path in paths.iter().filter(|x| x.extension().unwrap()=="bin"){
         let file = 
             fs::File::open(img_path)
             .expect("Can't Open File!!");
-        let mut reader = BufReader::new(file);
         let mut binary_data:Vec<u8> = Vec::new();
-        let binary_data_size = reader.read_to_end(&mut binary_data).expect("Can't Read File!!");
-        let mut binary_data = &binary_data[..];
-        println!("{:?}",binary_data_size);
-        let mut binary_imgs:Vec<BinaryImg> = 
-            Vec::with_capacity(binary_data_size/((1+1024*3)*8));
-        while !binary_data.is_empty(){
-            let img = unsafe{
-                let mut tmp:BinaryImg;
-                tmp = mem::uninitialized();
-                binary_data.read_exact(&mut tmp.label).unwrap();
-                binary_data.read_exact(&mut tmp.red).unwrap();
-                binary_data.read_exact(&mut tmp.green).unwrap();
-                binary_data.read_exact(&mut tmp.blue).unwrap();
-                tmp
-            };
-            binary_imgs.push(img);
-        }
-        imgs.push(binary_imgs);
+        BufReader::new(file)
+            .read_to_end(&mut binary_data)
+            .expect("Can't Read File!!");
+        binary_datas.append(&mut binary_data);
     }
-
-    0
+    let mut cifar_images:Vec<CifarImage> = Vec::new();
+    {
+        let mut binary_datas = &binary_datas[..];
+        while !binary_datas.is_empty(){
+            let (label,img) = unsafe{
+                use  image::GenericImage;
+                let mut label:[u8;1] = mem::uninitialized();
+                let mut red:[u8;1024] = mem::uninitialized();
+                let mut green:[u8;1024] = mem::uninitialized();
+                let mut blue:[u8;1024] = mem::uninitialized();
+                binary_datas.read_exact(&mut label).unwrap();
+                binary_datas.read_exact(&mut red).unwrap();
+                binary_datas.read_exact(&mut green).unwrap();
+                binary_datas.read_exact(&mut blue).unwrap();
+                let mut img = image::DynamicImage::new_rgb8(32,32);
+                for y in 0..32{
+                    for x in 0..32{
+                        let i = x + y * 32;
+                        let mut pixel:image::Rgba<u8> = mem::uninitialized();
+                        pixel.data = [red[i],green[i],blue[i],255];
+                        img.unsafe_put_pixel(x as u32 ,y as u32 ,pixel);
+                    }
+                }
+                (label[0],img)
+            };
+            let tmp = CifarImage{
+                label: meta_data[label as usize].clone(),
+                image: img
+            };
+            cifar_images.push(tmp);
+        }
+    }
+    cifar_images
 }
 
 fn main(){
@@ -62,5 +83,10 @@ fn main(){
     let path = std::path::Path::new(PATH);
     let args = std::env::args().collect::<Vec<String>>();
     println!("{:?}",args);
-    read_cifar(path);
+    let cifar_images = read_cifar(path);
+    let img = cifar_images[500].image.resize(500,500,image::FilterType::Lanczos3);
+    println!("{}",cifar_images[500].label);
+    let ref mut fout = std::fs::File::create(&std::path::Path::new("test.png")).unwrap();
+    let _ = img.save(fout, image::PNG).unwrap();
+    ()
 }
