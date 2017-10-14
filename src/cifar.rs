e4226b054479a888bf5607a8a360e945598da189
29rou <g196010r@icu.ac.jp>
2017-10-14 19:47:19 +0900

    minor momodifications

--- src/cifar.rs
+++ src/cifar.rs
@@ -1,10 +1,12 @@
 extern crate walkdir;
 extern crate image;
+extern crate rand;
 
 use std;
 use std::collections::LinkedList;
 
 pub struct CifarDataset {
+    pub count: usize,
     pub labels: Vec<String>,
     pub dataset: Vec<CifarImage>
 }
@@ -18,9 +20,10 @@ impl CifarDataset{
     pub fn new(path: &std::path::Path) -> Self{
         let ref paths = CifarDataset::get_file_paths(path);
         let meta_data = CifarDataset::get_meta_data(paths);
-        let binary_datas = CifarDataset::get_byte_datas(paths);
-        let cifar_images = CifarDataset::get_images(binary_datas);
-        CifarDataset{labels:meta_data ,dataset:cifar_images}
+        let byte_datas = CifarDataset::get_byte_datas(paths);
+        let cifar_images = CifarDataset::get_images(byte_datas);
+        let count =  cifar_images.len();
+        CifarDataset{labels:meta_data ,dataset:cifar_images,count:count}
     }
     fn get_file_paths(path: &std::path::Path) -> LinkedList<std::path::PathBuf>{
         walkdir::WalkDir::new(path)
@@ -32,30 +35,37 @@ impl CifarDataset{
     }
     fn get_meta_data(paths: &LinkedList<std::path::PathBuf>) -> Vec<String>{
         use std::io::Read;
-        let meta_path= paths
-            .iter()
-            .find(|x| x.extension().unwrap()=="txt")
-            .expect("Can't Find MetaFile!!");
-        let mut lines = String::new();
-        std::fs::File::open(meta_path)
-            .expect("Cant Open MetaFile!!")
-            .read_to_string(&mut lines)
-            .expect("Cant Read MetaFile!!");
-        lines.lines()
-            .map(|x| x.to_string())
-            .filter(|x| !x.is_empty())
-            .collect::<Vec<String>>()
+        paths.iter()
+            .filter(|x| x.extension().expect("Can't Find MetaFile!!")=="txt")
+            .map(|meta_path| ->String{
+                let mut lines = String::new();
+                std::fs::File::open(meta_path)
+                    .expect("Cant Open MetaFile!!")
+                    .read_to_string(&mut lines)
+                    .expect("Cant Read MetaFile!!");
+                lines
+            })
+            .map(|lines| -> Vec<String>{
+                lines.lines()
+                    .map(|x| x.to_string())
+                    .filter(|x| !x.is_empty())
+                    .collect::<Vec<String>>()
+            })
+            .fold(Vec::new(),|mut sum,ref mut labels| -> Vec<String>{
+                sum.append(labels);
+                sum
+            })
     }
     fn get_byte_datas(paths: &LinkedList<std::path::PathBuf>) -> Vec<Vec<u8>>{
         use std::io::{BufReader, Read};
         paths.iter()
-            .filter(|x| x.extension().unwrap()=="bin")
+            .filter(|x| x.extension().expect("Can't Find Bin File!!")=="bin")
             .map(|x| -> Vec<u8>{
-                let file = std::fs::File::open(x).expect("Can't Open File!!");
+                let file = std::fs::File::open(x).expect("Can't Open Bin File!!");
                 let mut binaray_data:Vec<u8> = Vec::new();
                 BufReader::new(file)
                     .read_to_end(&mut binaray_data)
-                    .expect("Can't Read File!!");
+                    .expect("Can't Read Bin File!!");
                 binaray_data
             })
             .map(|x| -> Vec<Vec<u8>>{
@@ -70,13 +80,24 @@ impl CifarDataset{
         byte_datas
             .into_iter()
             .map(move |x|{
-                std::thread::spawn(move ||{
-                    let img = CifarImage::new(& x);
-                    img
-                })
+                std::thread::spawn(move ||
+                    CifarImage::new(& x)
+                )
             })
-            .map(|x| x.join().expect("Tread Error!!"))
+            .map(|x| x.join().expect("Thread Error!!"))
             .collect()   
+    }
+    pub fn for_test_get_image_by_save(&self){
+        use self::rand::{thread_rng, Rng};
+        let ref nth:usize = thread_rng().gen_range(0,self.count);
+        let ref data = self.dataset[*nth];
+        let ref mut fout = 
+            std::fs::File::create(&std::path::Path::new("test.jpeg"))
+            .expect("Can't Ready To Save Image!!");
+        data.image
+            .resize(500,500,image::FilterType::Lanczos3)
+            .save(fout, image::JPEG).expect("Can't Save Image!!");
+        println!("No.{} {}",nth,self.labels[data.label as usize]);
     }
 }
 
@@ -88,7 +109,7 @@ impl CifarImage{
         let ref mut bytes:&[u8] = bytes.as_ref();
         let label = unsafe{
             let ref mut label:[u8;1] = mem::uninitialized();
-            bytes.read_exact(label).unwrap();
+            bytes.read_exact(label).expect("Can't Read Label!!");
             *label.get_unchecked(0)
         };
         let img = unsafe{
@@ -96,9 +117,9 @@ impl CifarImage{
             let ref mut red:[u8;1024] = mem::uninitialized();
             let ref mut green:[u8;1024] = mem::uninitialized();
             let ref mut blue:[u8;1024] = mem::uninitialized();
-            bytes.read_exact(red).unwrap();
-            bytes.read_exact(green).unwrap();
-            bytes.read_exact(blue).unwrap();
+            bytes.read_exact(red).expect("Can't Read Red!!");
+            bytes.read_exact(green).expect("Can't Read Green!!");
+            bytes.read_exact(blue).expect("Can't Read Blue!!");
             for y in 0..32{
                 for x in 0..32{
                     let i = x + y * 32;
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
-
-extern crate image;
 #[macro_use]
 extern crate lazy_static;
 
 mod cifar;
 
+lazy_static!{
+    static ref CIFARDATASET:cifar::CifarDataset = {
+        //let args = &std::env::args().collect::<Vec<String>>();
+        //println!("{:?}",args);
+        const PATH: &str = "./cifar-10-batches-bin/";
+        let path:&std::path::Path = std::path::Path::new(PATH);
+        //let path:&std::path::Path = std::path::Path::new(&args[1]);
+        cifar::CifarDataset::new(path)
+    };
+}
+
 fn main(){
-    const PATH: &str = "./cifar-10-batches-bin/";
-    let args = std::env::args().collect::<Vec<String>>();
-    println!("{:?}",args);
-    lazy_static!{
-        static ref CIFAR_DATASET:cifar::CifarDataset = {
-            let path = std::path::Path::new(PATH);
-            cifar::CifarDataset::new(path)
-        };
-    }
-    let img = CIFAR_DATASET.dataset[1023].image.resize(500,500,image::FilterType::Lanczos3);
-    let ref mut fout = std::fs::File::create(&std::path::Path::new("test.png")).unwrap();
-    let _ = img.save(fout, image::PNG).unwrap();
-    ()
+    let ref cifar_dataset = CIFARDATASET;
+    println!("Count:{}",cifar_dataset.count);
+    cifar_dataset.for_test_get_image_by_save();
 }
