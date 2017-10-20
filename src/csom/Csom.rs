4e4c654c60e576af2aa47acd89c38264e9754ea8
29rou <g196010r@icu.ac.jp>
2017-10-20 13:10:27 +0900

    add rand func as arg

--- src/csom/Csom.rs
+++ src/csom/Csom.rs
@@ -9,31 +9,32 @@ pub struct Csom {
 
 impl Csom {
     pub fn new(rng: &mut rand::ThreadRng) -> Self {
-        let som_layers = Csom::new_layer_init_by_rand((5, 256, 9), rng).unwrap();
+        use csom::rnd::{rand_0_1, rand_0_255};
+        let som_layers = Csom::new_layer_rand((5, 256, 9), rng, rand_0_255).unwrap();
         let fully_connected_layers: Vec<ndarray::Array2<f32>> = vec![
-            Csom::new_layer_init_by_rand((16, 16), rng).unwrap(),
-            Csom::new_layer_init_by_rand((16, 16), rng).unwrap(),
-            Csom::new_layer_init_by_rand((16, 10), rng).unwrap(),
-            Csom::new_layer_init_by_rand((10, 10), rng).unwrap(),
+            Csom::new_layer_rand((16, 16), rng, rand_0_1).unwrap(),
+            Csom::new_layer_rand((16, 16), rng, rand_0_1).unwrap(),
+            Csom::new_layer_rand((16, 10), rng, rand_0_1).unwrap(),
+            Csom::new_layer_rand((10, 10), rng, rand_0_1).unwrap(),
         ];
         Csom {
             som_layers: ::std::sync::RwLock::new(som_layers),
             fully_connected_layers: ::std::sync::RwLock::new(fully_connected_layers),
         }
     }
-    fn new_layer_init_by_rand<T, E>(
+    fn new_layer_rand<T, E>(
         shape: E,
         rng: &mut rand::ThreadRng,
+        rand_func: fn(&mut rand::ThreadRng) -> T,
     ) -> Result<ndarray::Array<T, <E as ndarray::IntoDimension>::Dim>, self::ndarray::ShapeError>
     where
         T: self::num::cast::FromPrimitive,
         E: self::ndarray::IntoDimension,
     {
         use ndarray::{Array, Dimension};
-        use csom::rnd::rand_0_255;
         let shape = shape.into_dimension();
         let num: usize = shape.slice().iter().fold(1, |s, x| s * x);
-        Array::from_iter((0..num).map(|_| rand_0_255(rng))).into_shape(shape)
+        Array::from_iter((0..num).map(|_| rand_func(rng))).into_shape(shape)
     }
     pub fn train(
         &self,
